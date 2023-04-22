e2e6b40b394a25dee3fb8c58a3166489b1f90ec5
Sara-MF <sara.melo@aluno.uece.br>
2023-04-22 10:17:53 -0300

    first commit

---
+++ resources/views/components/list.blade.php
@@ -0,0 +1,20 @@
+<div class=list-card>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">Imagem</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Eps</th>
+                <th scope="col">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Imagem</td>
+                <td>Nome</td>
+                <td>Eps</td>
+                <td>Status</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
---
+++ resources/views/components/login-card.blade.php
@@ -0,0 +1,6 @@
+<div class=login_card>
+    <input type="email" placeholder="E-mail">
+    <input type="text" placeholder="Senha">
+    <a href="/">Esqueceu a senha?</a>
+    <button>Entrar</button>
+</div>
