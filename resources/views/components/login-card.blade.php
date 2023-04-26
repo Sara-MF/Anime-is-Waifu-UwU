22adec480d27f153d53eec235f9da903564ca16f
Sara-MF <sara.melo@aluno.uece.br>
2023-04-26 20:37:06 -0300

    Changing login card name

--- resources/views/components/login-card.blade.php
+++ resources/views/components/login-card.blade.php
@@ -1,5 +1,5 @@
 <div class=login_card>
-    <h1 class="title">Anime is Waifu</h1>
+    <h1 class="title">Anime is Waifu UwU</h1>
     <input type="email" class="input-login" placeholder="E-mail">
     <input type="text" class="input-login" placeholder="Senha">
     <a href="/" class="forget-password">Esqueceu a senha?</a>
